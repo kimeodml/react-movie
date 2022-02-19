25cb32064b2d59941a1185b40c7dfa97c0cf91d7
kimeodml <kimeodml@gmail.com>
2022-02-19 18:03:56 +0900

    Add signin

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Home from "./Home";
 import { SValidation } from "./Validation";
 
 function SignIn() {
@@ -9,7 +10,7 @@ function SignIn() {
     name: "",
     gender: "default",
   });
-
+  const [newUser, setNewUser] = useState(false);
   const [errors, setErrors] = useState({});
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -21,76 +22,84 @@ function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setErrors(SValidation(text));
+    console.log(text);
+    setNewUser(true);
   };
   return (
-    <div className="contents">
-      <form className="form">
-        <h1 className="title">회원가입</h1>
-        <div className="group">
-          <label className="label">아이디</label>
-          <input
-            className="input"
-            type="text"
-            name="id"
-            value={text.id}
-            onChange={handleChange}
-          />
-          {errors.id && <p className="error">{errors.id}</p>}
+    <>
+      {newUser ? (
+        <Home />
+      ) : (
+        <div className="contents">
+          <form className="form">
+            <h1 className="title">회원가입</h1>
+            <div className="group">
+              <label className="label">아이디</label>
+              <input
+                className="input"
+                type="text"
+                name="id"
+                value={text.id}
+                onChange={handleChange}
+              />
+              {errors.id && <p className="error">{errors.id}</p>}
+            </div>
+            <div className="group">
+              <label className="label">비밀번호</label>
+              <input
+                className="input"
+                type="password"
+                name="password"
+                value={text.password}
+                onChange={handleChange}
+              />
+              {errors.password && <p className="error">{errors.password}</p>}
+            </div>
+            <div className="group">
+              <label className="label">비밀번호 재확인</label>
+              <input
+                className="input"
+                type="password"
+                name="password2"
+                value={text.password2}
+                onChange={handleChange}
+              />
+              {errors.password2 && <p className="error">{errors.password2}</p>}
+            </div>
+            <div className="group">
+              <label className="label">이름</label>
+              <input
+                className="input"
+                type="name"
+                name="name"
+                value={text.name}
+                onChange={handleChange}
+              />
+              {errors.name && <p className="error">{errors.name}</p>}
+            </div>
+            <div className="group">
+              <label className="label">성별</label>
+              <br />
+              <select
+                className="input"
+                name="gender"
+                value={text.gender}
+                onChange={handleChange}
+              >
+                <option value="default"> 성별</option>
+                <option value="1">남성</option>
+                <option value="2">여성</option>
+                <option value="3">선택안함</option>
+              </select>
+              {errors.gender && <p className="error">{errors.gender}</p>}
+            </div>
+            <button className="btn" type="submit" onClick={handleSubmit}>
+              가입하기
+            </button>
+          </form>
         </div>
-        <div className="group">
-          <label className="label">비밀번호</label>
-          <input
-            className="input"
-            type="password"
-            name="password"
-            value={text.password}
-            onChange={handleChange}
-          />
-          {errors.password && <p className="error">{errors.password}</p>}
-        </div>
-        <div className="group">
-          <label className="label">비밀번호 재확인</label>
-          <input
-            className="input"
-            type="password"
-            name="password2"
-            value={text.password2}
-            onChange={handleChange}
-          />
-          {errors.password2 && <p className="error">{errors.password2}</p>}
-        </div>
-        <div className="group">
-          <label className="label">이름</label>
-          <input
-            className="input"
-            type="name"
-            name="name"
-            value={text.name}
-            onChange={handleChange}
-          />
-          {errors.name && <p className="error">{errors.name}</p>}
-        </div>
-        <div className="group">
-          <label className="label">성별</label>
-          <br />
-          <select
-            className="input"
-            name="gender"
-            value={text.gender}
-            onChange={handleChange}
-          >
-            <option value="default"> 성별</option>
-            <option value="1">남성</option>
-            <option value="2">여성</option>
-            <option value="3">선택안함</option>
-          </select>
-          {errors.gender && <p className="error">{errors.gender}</p>}
-        </div>
-        <button className="btn" type="submit" onClick={handleSubmit}>
-          가입하기
-        </button>
-      </form>
-    </div>
+      )}
+    </>
   );
 }
 
