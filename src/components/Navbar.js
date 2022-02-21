1f813cf7f3e221201cbdffd17ecd45897f3a0065
kimeodml <kimeodml@gmail.com>
2022-02-21 16:37:18 +0900

    Change navbar layout
    Add span tag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
@@ -7,44 +8,27 @@ function Navbar() {
         <h1>
           <Link to="/">Logo</Link>
         </h1>
-        <ul className="userInfo">
-          <li>
+        <div className="user_info">
+          <span className="user_list">
             <Link to="/login">로그인</Link>
-          </li>
-        </ul>
-        <ul>
-          <Link to="/signin">회원가입</Link>
-        </ul>
+          </span>
+          <span className="user_list">
+            <Link to="/signup">회원가입</Link>
+          </span>
+        </div>
       </div>
       <div className="content_bottom">
-        <ul className="nav-menu">
-          <li>
-            <Link to="/rommance">로맨스</Link>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <Link to="/document">다큐</Link>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <Link to="/musical">뮤지컬</Link>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <Link to="/animation">애니메이션</Link>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <Link to="/review">자유 게시판</Link>
-          </li>
-        </ul>
-        <ul>
-          <input type="text" placeholder="검색" />
-        </ul>
+        <div className="nav-menu">
+          <Link to="/movie/genre/rommance">로맨스</Link>
+          <Link to="/movie/genre/document">다큐</Link>
+          <Link to="/movie/genre/musical">뮤지컬</Link>
+          <Link to="/movie/genre/musical">뮤지컬</Link>
+          <Link to="/movie/genre/animation">애니메이션</Link>
+          <Link to="/review">자유 게시판</Link>
+          <span className="search">
+            <input type="text" placeholder="검색" />
+          </span>
+        </div>
       </div>
     </div>
   );
