37b00acd6f4b6b5d7e7afffe31001c0606e1e081
kimeodml <kimeodml@gmail.com>
2022-02-24 12:33:38 +0900

    Correct documentary

--- src/List.js/MovieList.js
+++ src/List.js/MovieList.js
@@ -1,7 +1,7 @@
 export const MovieList = [
   { title: "로맨스", path: "genre=romance" },
   { title: "액션", path: "genre=action" },
-  { title: "다큐멘터리", path: "genre=documantary" },
+  { title: "다큐멘터리", path: "genre=documentary" },
   { title: "뮤지컬", path: "genre=music" },
   { title: "애니메이션", path: "genre=animation" },
 ];
