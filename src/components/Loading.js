dc7fbc1cadbcea54bbc7495ebcfe8f77ac9bcd8e
kimeodml <kimeodml@gmail.com>
2022-02-25 20:12:58 +0900

    Change loading css

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,8 +3,11 @@ import styled from "styled-components";
 const Waiting = styled.h1`
   color: #e5e3c9;
   font-weight: bold;
-  text-align: center;
-  align-items: center;
+  font-style: italic;
+  margin: 0 auto;
+  position: absolute;
+  right: 50%;
+  top: 50%;
 `;
 
 function Loading() {
