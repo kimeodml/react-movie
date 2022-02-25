6a9264fb9b2aea8b290de81df4de2baee6a84ed4
kimeodml <kimeodml@gmail.com>
2022-02-25 20:13:46 +0900

    Add MovieDetail css

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Loading from "../components/Loading";
+import "./MovieDetail.css";
 
 function MovieDetail() {
   const { genre, page, id } = useParams();
@@ -23,16 +24,25 @@ function MovieDetail() {
         <Loading />
       ) : (
         <div key={movie.id} className="movie_detail">
-          <img src={movie.medium_cover_image} alt={movie.title} />
-          <h1>
-            <a href={`${movie.url}`} target="_blank" rel="noopener noreferrer">
-              {movie.title}
-            </a>
-          </h1>
-          <p>
-            {movie.rating}점 | {movie.year}년 | {movie.runtime}분
-          </p>
-          <p>{movie.description_intro}</p>
+          <div className="detail_text1">
+            <img src={movie.medium_cover_image} alt={movie.title} />
+            <div className="detail_text2">
+              <h1 className="detail_title">
+                <a
+                  href={`${movie.url}`}
+                  className="link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {movie.title}
+                </a>
+              </h1>
+              <p>{movie.rating}점</p>
+              <p>{movie.year}년</p>
+              <p>{movie.runtime}분</p>
+            </div>
+          </div>
+          <p className="detail_description">{movie.description_intro}</p>
         </div>
       )}
     </div>
