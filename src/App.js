83352e1c0aa61385ac15abbf7325fc699c664b67
kimeodml <kimeodml@gmail.com>
2022-03-03 21:24:46 +0900

    Add rudux system

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import LogIn from "./pages/Login/LogIn";
@@ -8,8 +9,23 @@ import Review from "./pages/Review";
 import Movie from "./pages/Movie/Movie";
 import MovieDetail from "./pages/Movie/MovieDetail";
 import Search from "./pages/Search";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { authService } from "Fbase";
+import { Button, SetUser } from "./redux/Actions";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(authService, (authUser) => {
+      if (authUser) {
+        dispatch(SetUser(authUser));
+      } else {
+        dispatch(SetUser(null));
+      }
+    });
+  }, [dispatch]);
+
   return (
     <Router>
       <Navbar />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import ReduxThunk from "redux-thunk";
+import rootReducer from "redux/RootReducer";
 import App from "./App";
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk, logger));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
