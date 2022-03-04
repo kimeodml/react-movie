e3f3b20c87712a147249212ed53d097ae3508e2c
kimeodml <kimeodml@gmail.com>
2022-03-04 15:43:15 +0900

    Change LOGIN to LOGOUT

--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -6,7 +6,7 @@ export const LOGIN = "LOGIN";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_ERROR = "LOGIN_ERROR";
 
-export const LOGOUT = "LOGIN";
+export const LOGOUT = "LOGOUT";
 export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
 export const LOGOUT_ERROR = "LOGOUT_ERROR";
 
